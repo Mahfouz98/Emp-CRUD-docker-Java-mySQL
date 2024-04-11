420cd95c50e3e3b03d892b29f71316d78a9f528d
loco <mahfouz1231@hotmail.com>
2024-04-11 23:49:34 +0200

    changing bind address for mysql

--- mysql_script.sh
+++ mysql_script.sh
@@ -3,3 +3,4 @@ sudo apt-get update
 sudo apt install mariadb-server -y
 sudo systemctl enable mariadb --now
 sudo mariadb --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';"
+sudo bash -c 'echo "bind-address = 0.0.0.0" >> /etc/mysql/mariadb.conf.d/60-galera.cnf '
