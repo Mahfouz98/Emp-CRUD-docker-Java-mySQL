0b05f66310ce298e2f5cc748de31a5c6dbe96b1c
loco <mahfouz1231@hotmail.com>
2024-04-12 00:23:36 +0200

    mysql script

--- mysql_script.sh
+++ mysql_script.sh
@@ -3,8 +3,8 @@ sudo apt-get update
 sudo apt install mariadb-server -y
 sudo systemctl enable mariadb --now
 sudo mariadb --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';"
-sudo mariadb --execute="CREATE USER 'ahmed'@'%' IDENTIFIED BY 'mahfouz123'; GRANT ALL PRIVILEGES ON userdb.* TO 'ahmed'@'%';"
-sudo mariadb --execute="FLUSH PRIVILEGES;"
+sudo mariadb -proot --execute="CREATE USER 'ahmed'@'%' IDENTIFIED BY 'mahfouz123'; GRANT ALL PRIVILEGES ON userdb.* TO 'ahmed'@'%';"
+sudo mariadb -proot --execute="FLUSH PRIVILEGES;"
 sudo bash -c 'echo "bind-address = 0.0.0.0" >> /etc/mysql/mariadb.conf.d/60-galera.cnf '
 sudo systemctl restart mariadb
 sudo ufw allow in on eth0 to any port 3306
