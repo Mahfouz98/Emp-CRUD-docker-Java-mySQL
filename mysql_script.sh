c3e56917891e3d4117f1f9d32f8af86633509bf7
loco <mahfouz1231@hotmail.com>
2024-04-12 08:33:31 +0200

    changing mysql script

--- mysql_script.sh
+++ mysql_script.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt install mariadb-server -y
 sudo systemctl enable mariadb --now
 sudo mariadb --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';"
-sudo mariadb -proot --execute="CREATE USER 'ahmed'@'%' IDENTIFIED BY 'mahfouz123'; GRANT ALL PRIVILEGES ON userdb.* TO 'ahmed'@'%';"
+sudo mariadb -proot --execute="CREATE USER 'ahmed'@'%' IDENTIFIED BY 'mahfouz123'; GRANT ALL PRIVILEGES ON employees.* TO 'ahmed'@'%';"
 sudo mariadb -proot --execute="FLUSH PRIVILEGES;"
 sudo bash -c 'echo "bind-address = 0.0.0.0" >> /etc/mysql/mariadb.conf.d/60-galera.cnf '
 sudo systemctl restart mariadb
